0b87cde8da951308591dd9650646557ac5634604
Uforo Ibenge <benjamin.uforo@gmail.com>
2020-09-28 18:38:48 +0100

    add -- table fixes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import DashboardSideBar from './DashboardSidebar'
+
 import style from "../assets/RouteStyle/layout.module.scss"
+import DashboardTopBar from './DashboardTopBar'
 
 export default function Layout({children}) {
     return (
-        
+        <>
+        <DashboardTopBar/>
         <div className={style["container"]}>
             <DashboardSideBar/>
             <main>
                 {children}
             </main>
         </div>
+        </>
     )
 }
--- src/routes/admin/Dashboard.jsx
+++ src/routes/admin/Dashboard.jsx
@@ -4,47 +4,71 @@ import style from "../../assets/RouteStyle/dashboard.module.scss";
 // import Table from "../../components/Table";
 import Students from "../../components/Students";
 import useTable from "../../components/Table";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSync,faQuestionCircle,faList,faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSync,
+  faQuestionCircle,
+  faList,
+  faPlusCircle,
+  faUsers,
+  faBook,
+  faUserGraduate,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Dashboard() {
   const topSection = [
     {
-      icon: "",
+      icon: <FontAwesomeIcon icon={faUsers} />,
       name: "Teachers",
       amt: "70000",
     },
     {
-      icon: "",
+      icon: <FontAwesomeIcon icon={faUserGraduate} />,
       name: "students",
       amt: "70000",
     },
     {
-      icon: "",
+      icon: <FontAwesomeIcon icon={faBook} />,
       name: "Courses",
       amt: "70000",
     },
     {
-      icon: "",
+      icon: <FontAwesomeIcon icon={faQuestionCircle} />,
       name: "Questions",
       amt: "70000",
     },
   ];
 
-  const tableHead = ["Name", "Course", "Location"];
+  const tableHead = ["Teacher ID", "First Name", "Last Name", "Email"];
+  const RegisteredStudentsTable = [
+    "Student ID",
+    "First Name",
+    "Last Name",
+    "Email",
+  ];
+  const uploadedCourses = ["Course ID", "Course title", "Course Code"];
+  // const tableHead = ["Teacher ID", "First Name", "Last Name", "Email"];
 
-  const tablebody = [
-    // { name: "Ade", course: "MHS 600", location: "Lagos" },
-    // { name: "Basit", course: "MHS 600", location: "abuja" },
-    // { name: "Josh", course: "MHS 702", location: "jos" },
+  const studentTablebody = [
+    { studentId: "001", firstName: "MHS 600", lastName:"adegbite", email: "Victoria@mail " },
+    { studentId: "002", firstName: "MHS 600", lastName:"adegbite", email: "abuja@email" },
+    { studentId: "003", firstName: "MHS 702", lastName:"adegbite", email: "jos@email" },
   ];
 
-  const keys = ["name","course", "location"]
-  const Table = useTable(tableHead,tablebody,keys);
+  
+  const tablebody = [
+    { name: "Ade", course: "MHS 600", email: "Lagos" },
+    { name: "Basit", course: "MHS 600", email: "abuja" },
+    { name: "Josh", course: "MHS 702", email: "jos" },
+  ];
 
+  const keys = ["studentId", "firstName", "lastName", "email"];
+  const Table = useTable(tableHead, tablebody, keys);
+  const studentTable = useTable(RegisteredStudentsTable, studentTablebody, keys);
+  const uploadTable = useTable(uploadedCourses, tablebody, keys);
+  // const Table = useTable(tableHead, tablebody, keys);
 
   // console.log(Table)
-
 
   return (
     <Layout>
@@ -54,7 +78,6 @@ export default function Dashboard() {
             <div key={10 * index} className={style["user__stats--item"]}>
               <div>
                 <span>{item.icon} </span>
-                <br />
                 <span>{item.name} </span>
               </div>
               <span className={style["amt"]}>{item.amt} </span>
@@ -65,44 +88,46 @@ export default function Dashboard() {
 
       {/* Tbody takes an array of key value pairs, key = table name and value = table data */}
 
-
       {Table}
-      <Misc/>
-      {Table}
-      {Table}
-      {Table}
-
-
-
+      <Misc />
+      {studentTable}
+      {uploadTable}
       {/* <Students /> */}
     </Layout>
   );
 }
 
-
 const Misc = () => {
   return (
     <section className={style["misc"]}>
-      <div  className={style["refresh"]}>
-        <header><FontAwesomeIcon icon={faSync}/></header>
+      <div className={style["refresh"]}>
+        <header>
+          <FontAwesomeIcon icon={faSync} />
+        </header>
 
         <span>Refresh all system data {">>"} click</span>
         <span>Refresh</span>
       </div>
-      <div  className={style["questions"]}>
+      <div className={style["questions"]}>
         {" "}
-        <header><FontAwesomeIcon icon={faQuestionCircle}/> 20 Questions</header>
+        <header>
+          <FontAwesomeIcon icon={faQuestionCircle} /> 20 Questions
+        </header>
         <span>Total Number of Available Questions</span>
         <span>Questions</span>
       </div>
-      <div  className={style["user__amt"]}>
+      <div className={style["user__amt"]}>
         {" "}
-        <header><FontAwesomeIcon icon={faList}/></header>
+        <header>
+          <FontAwesomeIcon icon={faList} />
+        </header>
         <span>Total Number of teachers</span>
         <span>Teachers</span>
       </div>
-      <div  className={style["courses"]}>
-        <header><FontAwesomeIcon icon={faPlusCircle}/></header>
+      <div className={style["courses"]}>
+        <header>
+          <FontAwesomeIcon icon={faPlusCircle} />
+        </header>
         <span>Total Number of available Course</span>
         <span>Add More</span>
       </div>
